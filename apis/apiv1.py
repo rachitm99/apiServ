b4c10af4ac67cecf59c0d4373162ad0a07e6c263
rachitm99 <rachitmukherjee99@gmail.com>
2023-03-26 16:25:18 +0530

    inittail commit

--- apis/apiv1.py
+++ apis/apiv1.py
@@ -1,11 +1,12 @@
 from flask import Blueprint ,request , jsonify
 from flask_restful import Api, Resource
-
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 API_KEY = "my_secret_key"
 device_info = Blueprint("device",__name__)
 
 class device(Resource):
+    @jwt_required
     def get(self):
         api_key = request.headers.get('X-API-Key')
         if api_key != API_KEY:
