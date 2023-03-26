968693a76acf7b1957e4d296c7e5f4b6bf0d21a9
rachitm99 <rachitmukherjee99@gmail.com>
2023-03-26 16:28:22 +0530

    inittail commit

--- apis/apiv1.py
+++ apis/apiv1.py
@@ -8,9 +8,6 @@ device_info = Blueprint("device",__name__)
 class device(Resource):
     @jwt_required
     def get(self):
-        api_key = request.headers.get('X-API-Key')
-        if api_key != API_KEY:
-            return {'message': 'API key authentication required'}, 401
         data = {"message": "RTX 4090"}
         return jsonify(data)
 
